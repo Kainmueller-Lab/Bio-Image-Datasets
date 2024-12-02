35d99e687fa8705cee3591b9c52617f0fe831711
NoraKrbr <nora.koreuber@mdc-berlin.de>
2024-12-02 15:09:41 +0100

    pannuke dataset start

--- src/bio_image_datasets/pannuke.py
+++ src/bio_image_datasets/pannuke.py
@@ -2,15 +2,14 @@ import os
 import h5py
 import numpy as np
 from bio_image_datasets.dataset import Dataset
-from skimage.measure import label as relabel
 
 mapping_dict = {
-            0: "Background",
-            1: "Epithelial",
-            2: "Dead Cells",
-            3: "Connective/Soft tissue cells",
-            4: "Inflammatory",
-            5: "Neoplastic cells",
+            0: "Neoplastic cells",
+            1: "Inflammatory",
+            2: "Connective/Soft tissue cells",
+            3: "Dead Cells",
+            4: "Epithelial",
+            6: "Background",
         }
 
 
@@ -24,19 +23,19 @@ class PanNukeDataset(Dataset):
             local_path (str): Path to the directory containing the files.
         """
         # get fold number from local_path
-        if 'fold1' in local_path:
+        if local_path == '/fast/AG_Kainmueller/data/pannuke/fold1':
             fold = 1
-        elif 'fold2' in local_path:
+        elif local_path == '/fast/AG_Kainmueller/data/pannuke/fold2':
             fold = 2
-        elif 'fold3' in local_path:
+        elif local_path == '/fast/AG_Kainmueller/data/pannuke/fold3':
             fold = 3
         else:
             raise ValueError("Invalid local path.")
 
         super().__init__(local_path)
-        self.images_file = os.path.join(local_path, f'images/fold{fold}/images.npy')
-        self.types_file = os.path.join(local_path, f'images/fold{fold}/types.npy')
-        self.masks_file = os.path.join(local_path, f'masks/fold{fold}/masks.npy')
+        self.images_file = os.path.join(local_path, f'/images/fold{fold}/images.npy')
+        self.types_file = os.path.join(local_path, f'/images/fold{fold}/types.npy')
+        self.masks_file = os.path.join(local_path, f'/masks/fold{fold}/masks.py')
 
         if not self.images_file:
             raise ValueError("No images file found in the specified directory.")
@@ -47,19 +46,11 @@ class PanNukeDataset(Dataset):
         if not self.masks_file:
             raise ValueError("No masks file found in the specified directory.")
 
-        # put last channel in images to first
-        self.images = np.load(self.images_file)
-        self.images = np.moveaxis(self.images, -1, 1)
-        self.types = np.load(self.types_file)
-        self.masks = np.load(self.masks_file)
-
-        self.semantic_masks = self.prepare_semantic_masks(self.masks)
-        self.instance_masks = self.prepare_instance_masks(self.masks)
-
 
     def __len__(self):
         """Return the number of samples in the dataset."""
-        return np.shape(self.types)[0]
+        types = np.load(self.types_file)
+        return np.shape(types)[0]
 
     def __getitem__(self, idx):
         """Return a sample as a dictionary at the given index.
@@ -68,21 +59,21 @@ class PanNukeDataset(Dataset):
             idx (int): Index of the sample.
         Returns:
             dict: A dictionary containing the following keys:
-                - "image": Hematoxylin and eosin (HE) image
-                - "type": Tissue type where the sample comes from
-                - "semantic_mask": Ground truth semantic mask
-                - "instance_mask": Ground truth instance mask
-                - "sample_name": Index of the sample as string
+                - "gt_ct": Ground truth cell type mask.
+                - "gt_inst": Ground truth instance mask.
+                - "he_img": Hematoxylin and eosin (HE) image
         """
         if idx >= len(self):
             raise IndexError("Index out of bounds.")
 
+        images = np.load(self.images_file)
+        types = np.load(self.types_file)
+        masks = np.load(self.masks_file)
+
         data = {
-            "image": self.images[idx],
-            "type": self.types[idx],
-            "semantic_mask": self.semantic_masks[idx],
-            "instance_mask": self.instance_masks[idx],
-            'sample_name': self.get_sample_name(idx)
+            "type": types[idx],
+            "gt_inst": masks[idx],
+            "he_img": image[idx],
         }
         return data
 
@@ -94,17 +85,24 @@ class PanNukeDataset(Dataset):
         Returns:
             np.ndarray: The HE image.
         """
-        return self.images[idx]
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
 
-    def get_tissue_type(self, idx):
-        """Load tissue type.
+        images = np.load(self.images_file)
+
+    def get_if(self, idx):
+        """Load immunofluorescence (IFL) data for the given index.
         
         Args:
             idx (int): Index of the sample.
         Returns:
-            tissue type (string): The tissue type.
+            np.ndarray: The immunofluorescence data.
         """
-        return self.types[idx]
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
+        file_path = self.file_paths[idx]
+        with h5py.File(file_path, 'r') as f:
+            return np.squeeze(f["ifl"][:])
 
     def get_class_mapping(self):
         """Return the class mapping for the dataset.
@@ -122,7 +120,11 @@ class PanNukeDataset(Dataset):
         Returns:
             np.ndarray: The instance mask.
         """
-        return self.instance_masks[idx]
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
+        file_path = self.file_paths[idx]
+        with h5py.File(file_path, 'r') as f:
+            return np.squeeze(f["gt_inst"][:])
 
     def get_semantic_mask(self, idx):
         """Return the semantic mask at the given index.
@@ -132,8 +134,11 @@ class PanNukeDataset(Dataset):
         Returns:
             np.ndarray: The semantic mask.
         """
-        return self.semantic_masks[idx]
-
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
+        file_path = self.file_paths[idx]
+        with h5py.File(file_path, 'r') as f:
+            return np.squeeze(f["gt_ct"][:])
 
     def get_sample_name(self, idx):
         """Return the sample name for the given index.
@@ -143,38 +148,14 @@ class PanNukeDataset(Dataset):
         Returns:
             str: The sample name.
         """
-        return str(idx)
+        if idx >= len(self):
+            raise IndexError("Index out of bounds.")
+        return self.files[idx]
 
     def get_sample_names(self):
         """Return the list of all sample names."""
-        return [str(i) for i in range(int(len(self)))]
+        return self.files
 
     def __repr__(self):
         """Return the string representation of the dataset."""
-        return f"PanNuke Dataset ({self.local_path}, {len(self)} samples)"
-
-    def prepare_semantic_masks(self, masks):
-        """ Prepare the semantic segmentation mask based on the pannuke mask which contains each semantic class
-        in an individual channel which need to be collapsed via argmax
-
-        Args:
-            masks (np.array): B x H x W x C 
-        """
-        # reverse order of last dim of masks
-        masks = np.flip(masks, axis=-1)
-        semantic_masks = np.argmax(masks, -1)
-        return semantic_masks
-
-    def prepare_instance_masks(self, masks):
-        """ Prepare the instance segmentation mask based on the pannuke mask which contains each semantic class
-        in an individual channel which need to be collapsed via max
-
-        Args:
-            masks (np.array): B x H x W x C 
-        """
-        # reverse order of last dim of masks
-        instance_masks = np.max(masks[..., :-1], -1)
-        # iterate over first dimension and use relabel on each individual tile
-        for i in range(instance_masks.shape[0]):
-            instance_masks[i] = relabel(instance_masks[i], background=0)
-        return instance_masks
+        return f"SchuerchDataset ({self.local_path}, {len(self)} samples)"
